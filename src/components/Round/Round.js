ed0385ee5a1be1a49174b71ed53c680667ecc383
Laura Collins <laura.e.collins1@gmail.com>
2020-10-28 13:09:44 -0500

    improved score tracking

--- src/components/Round/Round.js
+++ src/components/Round/Round.js
@@ -13,13 +13,16 @@ function Round(props) {
             score={props.score}
             setScore={props.setScore}
             numRound={props.numRound}
+            setTotalScore={props.setTotalScore}
+            totalScore={props.totalScore}
             />)
             : (
             <div className="endOfGame mt-5 mb-2">
                 <h3>Congrats!</h3>
                 <h5>You have finished the game!</h5>
                 <h5>You have played {props.numRound} round&#40;s&#41;.</h5>
-                <h5>You got {props.score} out of {props.numRound}0 correct!</h5>
+                <h5>You got {props.score} out of 10 correct this round!</h5>
+                <h5>Your total score is {props.totalScore} out of {props.numRound}0 correct!</h5>
                 <button className="btn btn-info mt-5" onClick={() => props.resetGame()}>Play Again?</button>
             </div>)}
         </div>
